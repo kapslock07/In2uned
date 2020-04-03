6d171bc646d75caeb55c44b754877306b74a46f0
Chris Soltren <csoltren1@yahoo.com>
2020-04-02 20:29:46 -0400

    initial commit to new branch

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -35,12 +35,12 @@ module.exports = function (server) {
 
         let scopes = 'user-read-private user-read-email';
         let redirect_uri = "http://localhost:8080/callback";
-                            
-        res.redirect('https://accounts.spotify.com/authorize?client_id=' + process.env.API_CLIENT_ID + 
-        '&response_type=code&redirect_uri=' + encodeURIComponent(redirect_uri) + '&scope=' + encodeURIComponent(scopes));
+
+        res.redirect('https://accounts.spotify.com/authorize?client_id=' + process.env.API_CLIENT_ID +
+            '&response_type=code&redirect_uri=' + encodeURIComponent(redirect_uri) + '&scope=' + encodeURIComponent(scopes));
     });
 
-       
+
 
     server.get("/callback", (req, res) => {
 
@@ -48,10 +48,10 @@ module.exports = function (server) {
 
         let authCode = '';
 
-        if(reqURL.includes("/callback?code=")){
+        if (reqURL.includes("/callback?code=")) {
             authCode = reqURL.substring(15);
         }
-        
+
 
         res.render("index");
     });
