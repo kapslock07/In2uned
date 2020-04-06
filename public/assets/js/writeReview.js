2c7ac7a6ccb643728f6375aa601767d3dd2118c8
Chris Soltren <csoltren1@yahoo.com>
2020-04-06 13:15:56 -0400

    Still trying to get stars working, may just scrap and put numbers rating

--- public/assets/js/writeReview.js
+++ public/assets/js/writeReview.js
@@ -7,11 +7,17 @@ $(document).ready(() => {
     });
 });
 
-var ratingsField = $('#ratings-hidden');
-
-
-$('.starrr').on('starrr:change', function (e, value) {
-    ratingsField.val(value);
-});
+function add(ths, sno) {
+    for (var i = 1; i <= 5; i++) {
+        var cur = document.getElementById("star" + i)
+        cur.className = "far fa-star"
+    }
+    for (var i = 1; i <= sno; i++) {
+        var cur = document.getElementById("star" + i)
+        if (cur.className == "far fa-star") {
+            cur.className = "fas fa-star"
+        }
+    }
+}
 
 
