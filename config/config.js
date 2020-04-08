36661d58ecb0e2e572188481debd98d38cb8a2f9
Ryan Kaplan <ryankaplan7@gmail.com>
2020-04-07 21:52:49 -0400

    minor mo ile changes

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "iNotlNN4",
-    "database": "in2uned_db_local",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.LOCAL_DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql",
   },
