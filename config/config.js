8e895ad7151cb37c1626b397ccf5ebf9e889e9d9
KantKontrol <nick.derissio.tech@gmail.com>
2020-04-09 17:25:58 -0400

    changed position of About us in navbar

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": "in2uned_db_local",
     "host": "127.0.0.1",
-    "dialect": "mysql",
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
